722ea9163bc08aef13cc93f44a9acde93a691333
fmedinalTJSD <faustomedina88@gmail.com>
2025-11-10 00:03:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -341,9 +341,7 @@ def calculate():
         flash(trans["error_calc"], "danger")
         return redirect(url_for("index", lang=lang))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-
     if __name__ == "__main__":
     import os
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
